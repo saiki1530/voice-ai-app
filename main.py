c136c95e923902fab8fdb31b4b0d4019cd57fc51
Quang Sang <quang.sang1530@gmail.com>
2025-06-17 15:25:01 +0700

    Voice AI app

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from fastapi.staticfiles import StaticFiles
 from fastapi.concurrency import run_in_threadpool
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-API_KEY = os.getenv("GEMINI_API_KEY")
-genai.configure(api_key=API_KEY)
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 app = FastAPI()
 
@@ -19,19 +20,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def serve_frontend():
+    return FileResponse("static/index.html")
+
 class Prompt(BaseModel):
     prompt: str
+
 @app.post("/api/ask")
 async def ask(prompt: Prompt):
     try:
-        print("Prompt nhận được:", prompt.prompt)
         model = genai.GenerativeModel("gemini-1.5-flash")
         prompt_vi = f"Trả lời bằng tiếng Việt: {prompt.prompt}"
         response = await run_in_threadpool(model.generate_content, prompt_vi)
         ai_reply = response.candidates[0].content.parts[0].text
-        print("Kết quả trả về:", ai_reply)
-
         return {"answer": ai_reply}
     except Exception as e:
-        print("Lỗi:", str(e))
         return {"answer": f"Lỗi server: {str(e)}"}
